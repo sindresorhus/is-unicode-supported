31e9c934beb4160c4d671be5cf768f369ffdcf29
ehmicky <ehmicky@users.noreply.github.com>
2021-07-02 19:44:35 +0200

    Add support for ConEmu and cmder (#8)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export default function isUnicodeSupported() {
 
 	return Boolean(process.env.CI) ||
 		Boolean(process.env.WT_SESSION) || // Windows Terminal
+		process.env.ConEmuTask === '{cmd::Cmder}' || // ConEmu and cmder
 		process.env.TERM_PROGRAM === 'vscode' ||
 		process.env.TERM === 'xterm-256color' ||
 		process.env.TERM === 'alacritty';
